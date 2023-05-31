569e22194b9e32fb0f9ecef27f83f8795e2c42af
Dazcam <camerond@cardiff.ac.uk>
2023-05-31 16:47:05 +0100

    Add REST API functionality

--- R/get_mhc_genes.R
+++ R/get_mhc_genes.R
@@ -1,33 +1,75 @@
-#' Get vector of hg38 genes overlapping MHC region using biomaRt
+#' Get vector of hg38 genes overlapping MHC region using REST API or BiomaRt
 #'
-#' @return A vector of unique genes that fall within MHC region of chr6.
+#' @return A dataframe of genes, positions and metadata overlapping the MHC region of chr6.
 #' @export
 #'
-#' @param start Start position of MHC region.
-#' @param end End position of MHC region.
-#' @param server Ensembl server to use to call genes.
+#' @param build A genome build to query the MHC genes from (hg19 or hg38)
+#' @param method The query method to use (biomart o rest_api)
 #'
-#' @examples get_mhc_genes(start = 28510120, end = 33480577, server = "ensembl")
-get_mhc_genes <- function(
+#'
+#' @examples get_mhc_genes(build = 'hg38', method = 'rest_api)
+get_mhc_genes <- function(build = 'hg38', method = 'rest_api') {
 
-    start = "28510120",
-    end = "33480577",
-    server = "ensembl"
+  stopifnot(build == 'hg19' || build == 'hg38')
 
-    ) {
+  if (build == 'hg19') {
 
-  cat('\n\nConnecting to Ensembl server ...')
-  mart <- biomaRt::useMart(server)
-  mart <- biomaRt::useDataset("hsapiens_gene_ensembl", mart)
+    biomart_server <- "https://grch37.ensembl.org"
+    rest_server <- "https://grch37.rest.ensembl.org"
+    coord <- "chr6:28,477,797-33,448,354"
+    start <- "28477797"
+    end <- "33448354"
 
-  cat('\nCalling MHC region genes ...\n')
-  mhc_genes <- biomaRt::getBM(attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position"),
-                     filters = c("chromosome_name","start","end"),
-                     values = list(chromosome = "6", start = start, end = end,
-                     mart = mart))
-  mhc_genes_uniq <- stringi::stri_remove_empty(unique(mhc_genes$hgnc_symbol), na_empty = FALSE)
+
+  } else {
+
+    biomart_server <- "https://www.ensembl.org"
+    rest_server <- "https://rest.ensembl.org"
+    coord <- "chr6:28,510,120-33,480,577"
+    start = "28510120"
+    end = "33480577"
+
+  }
+
+  if (method == 'biomart') {
+
+
+  message('\n\nConnecting to Ensembl server via BiomaRt ...')
+  mart <- useEnsembl("ensembl", "hsapiens_gene_ensembl", host = biomart_server)
+
+  message('\nCalling MHC region genes ...\n')
+  mhc_genes <- biomaRt::getBM(attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position",
+                                             "gene_biotype", "source", "version", "ensembl_gene_id"),
+                     filters = c("chromosome_name", "start", "end"),
+                     values = list(chromosome = "6", start = start, end = end),
+                     mart = mart)
+  #mhc_genes_uniq <- stringi::stri_remove_empty(unique(mhc_genes$hgnc_symbol), na_empty = FALSE)
   cat('\n\nMHC genes detected:', length(mhc_genes_uniq), '\n\n')
 
-  return(mhc_genes_uniq)
+  return(mhc_genes)
+
+  }
+
+  if (method == 'rest_api') {
+
+
+    message('\n\nConnecting to Ensembl server via REST API...')
+    ext <- paste0("/overlap/region/human/", coord, "?feature=gene")
+    r <- GET(paste(rest_server, ext, sep = ""), content_type("text/csv"))
+    stop_for_status(r)
+
+    gene_list_df <- print(content(r)) %>%
+      map(unlist) %>%
+      map(t) %>%
+      map(as_tibble) %>%
+      bind_rows() %>%
+      relocate(external_name, seq_region_name, start,
+               end, assembly_name, biotype, description)
+
+    return(gene_list_df)
+
+  }
 
 }
+
+
