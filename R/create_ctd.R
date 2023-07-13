0aef237be7258f95990f34d7f2a12958caeb1c32
Dazcam <camerond@cardiff.ac.uk>
2023-07-13 15:00:52 +0100

    Change matrix to gex_matrix and tweak header

--- R/create_ctd.R
+++ R/create_ctd.R
@@ -7,39 +7,42 @@
 #' @importFrom EWCE generate_celltype_data drop_uninformative_genes
 #' @importFrom edgeR cpm
 #'
-#' @param matrix Raw count gene matrix - needs to be genes x cell and annotation data
-#' @param meta_lev_1 A vector of cell IDs for level 1 cell types
-#' @param meta_lev_2  A vector of cell IDs for level 1 cell types
-#' @param outdir The output directory to store the ctd object
-#' @param mhc_genes A set of MHC genes to be removed
-#' @param group_name An string identifier for the dataset
-#' @param threads Number of cores to assign to process. Default is NULL,
+#' @param gex_matrix A raw count gene expression matrix - needs to be genes x cell.
+#' @param meta_lev_1 A vector of cell-type labels for level 1 cell types.
+#' @param meta_lev_2  A vector of cell-type labels for level 2 cell types.
+#' @param outdir The output directory to store the ctd object.
+#' @param mhc_genes A vector of HGNC ID genes overlapping the MHC to be removed.
+#' @param group_name An unique identifier (string) for the dataset.
+#' @param threads The number of cores to assign to process. Default is NULL,
 #' which will auto detect total cores and estimate number to use.
 #'
-#' @returns directory where ctd object was stored
+#' @returns A path to the location where where the ctd object was stored.
 #'
 #' @examples
 #' data('seurat_small', 'mhc_genes')
-#' create_ctd(seurat_small@assays$RNA@data,
-#'                      meta_lev_1 = seurat_small$cluster_level_1,
-#'                      meta_lev_2 = seurat_small$cluster_level_2,
-#'                      outdir = 'test',
-#'                      mhc_genes = mhc_genes,
-#'                      group_name = 'brain_study',
-#'                      threads = 1)
+#' create_ctd(gex_matrix = seurat_small@assays$RNA@data,
+#'                     meta_lev_1 = seurat_small$cluster_level_1,
+#'                     meta_lev_2 = seurat_small$cluster_level_2,
+#'                     outdir = 'test',
+#'                     mhc_genes = mhc_genes,
+#'                     group_name = 'brain_study',
+#'                     threads = 1)
 
-create_ctd <- function(matrix = NULL, meta_lev_1 = NULL, meta_lev_2 = NULL,
+create_ctd <- function(gex_matrix = NULL, meta_lev_1 = NULL, meta_lev_2 = NULL,
                        outdir = NULL, mhc_genes = NULL, group_name = NULL,
                        threads = NULL) {
 
   message('\n\nCreating CTD object ... \n')
 
-  # Remove MHC genes from matrix
-  matrix_no_mhc <- matrix[!(rownames(matrix) %in% mhc_genes), ]
-  message('MHC genes removed: ', dim(matrix)[1] - dim(matrix_no_mhc)[1])
+  # Remove MHC genes from gex_matrix
+  gex_matrix_no_mhc <- gex_matrix[!(rownames(gex_matrix) %in% mhc_genes), ]
+  message('MHC genes removed: ', dim(gex_matrix)[1] - dim(gex_matrix_no_mhc)[1])
+
+  # Can also be done directly to seurat object!!
+  # gex_matrix_no_mhc <- seurat_obj[!(rownames(seurat_obj) %in% mhc_genes), ]
 
   # Create annotations
-  annotations <- as.data.frame(cbind(as.vector(colnames(matrix)),
+  annotations <- as.data.frame(cbind(as.vector(colnames(gex_matrix)),
                                      as.vector(meta_lev_1),
                                      as.vector(meta_lev_2)))
   colnames(annotations) <- c('cell_id', 'level1class', 'level2class')
@@ -52,7 +55,7 @@ create_ctd <- function(matrix = NULL, meta_lev_1 = NULL, meta_lev_2 = NULL,
   # COUNTS_SCT <- EWCE::sct_normalize(RAW_COUNTS_NO_MHC)
 
   message('\nRunning CPM ...')
-  matrix_cpm <- edgeR::cpm(matrix_no_mhc)
+  gex_matrix_cpm <- edgeR::cpm(gex_matrix_no_mhc)
 
   # cat('\nDropping uninformative genes raw ... ', '\n\n')
   # DROP_GENES_RAW <- EWCE::drop_uninformative_genes(
@@ -69,17 +72,17 @@ create_ctd <- function(matrix = NULL, meta_lev_1 = NULL, meta_lev_2 = NULL,
   #   level2annot = annotLevels$level2class)
 
   message('\nDropping uninformative genes cpm norm ...')
-  matrix_drop_genes <- EWCE::drop_uninformative_genes(
-    exp = matrix_cpm,
+  gex_matrix_drop_genes <- EWCE::drop_uninformative_genes(
+    exp = gex_matrix_cpm,
     input_species = "human",
     output_species = "human",
     level2annot = annotLevels$level2class,
     no_cores = threads)
 
   message('\nGene counts:',
-      '\n\nRAW:', dim(matrix)[1],
-      '\nRAW_NO_MHC:', dim(matrix_cpm)[1],
-      '\nCPM_DROP_GENES:', dim(matrix_drop_genes)[1])
+      '\n\nRAW:', dim(gex_matrix)[1],
+      '\nRAW_NO_MHC:', dim(gex_matrix_cpm)[1],
+      '\nCPM_DROP_GENES:', dim(gex_matrix_drop_genes)[1])
 
   # cat('\nGene counts:',
   #     '\n\nRAW:', dim(RAW_COUNTS)[1],
@@ -91,7 +94,7 @@ create_ctd <- function(matrix = NULL, meta_lev_1 = NULL, meta_lev_2 = NULL,
   # Create object - saves ctd obj to folder
   message('\nCreating CTD object ... \n\n')
   dir.create(outdir, showWarnings = FALSE)
-  ctd <- EWCE::generate_celltype_data(exp = matrix_drop_genes,
+  ctd <- EWCE::generate_celltype_data(exp = gex_matrix_drop_genes,
                                       annotLevels = annotLevels,
                                       groupName = group_name,
                                       savePath = outdir,
